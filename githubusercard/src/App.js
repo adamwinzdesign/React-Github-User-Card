f784ac938d30f1b80f68f0e3af5ad95afab8231c
adamwinzdesign <adamwinzdesign@gmail.com>
2019-12-03 20:12:00 -0600

    axios up

--- githubusercard/src/App.js
+++ githubusercard/src/App.js
@@ -1,12 +1,50 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Test</h1>
-    </div>
-  );
+import axios from 'axios';
+// import UserCard from './components/UserCard';
+// import FollowerCard from './components/FollowerCard';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      user: [],
+      follower: []
+    }
+    console.log('Constructor is up!');
+  }
+
+  componentDidMount() {
+    axios
+      .get('https://api.github.com/users/adamwinzdesign')
+      .then(res => {
+        console.log(res);
+        this.setState({
+          user: res.data
+        })
+      })
+      .catch(error => console.log(error.response));
+
+    axios
+      .get('https://api.github.com/users/adamwinzdesign/followers')
+      .then(res => {
+        console.log(res);
+        this.setState({
+          follower: res.data
+        })
+      })
+      .catch(error => console.log(error.response));
+  }
+
+  render() {
+    return(
+      <div>
+        {/* <UserCard user={this.state.user} />
+        <FollowerCard follower={this.state.follower} /> */}
+      </div>
+    )
+  }
 }
 
 export default App;
